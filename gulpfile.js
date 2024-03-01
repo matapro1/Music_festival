1dae5deb4ce74849b6555117f84d73c1c8b96dcf
mataProDigital <mataprousa@gmail.com>
2024-03-01 00:56:46 -0800

    sass, npm, npx, gulp

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+const { src, dest, watch } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+
+function css(done){
+  src("src/scss/app.scss") // Identificar el archivo SASS
+    .pipe(sass()) // Compilarlo
+    .pipe(dest("build/css"))  // Almacenarlo en el disco duro
+
+
+  done(); //Callback que avisa a gulp cuando llegamos al final
+}
+
+
+function dev(done){
+  watch("src/scss/app.scss", css)
+
+
+  done()
+}
+exports.css =css;
+exports.dev = dev;
